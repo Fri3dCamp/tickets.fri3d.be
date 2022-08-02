ef182c90b93ebd95522d450a69f213a00c2eef8a
jef <jef@codewerken.be>
2022-08-03 01:40:07 +0200

    nearer prod daemons

--- fr1ckets/src/fr1ckets/static/daemons/js/index.js
+++ fr1ckets/src/fr1ckets/static/daemons/js/index.js
@@ -1,56 +1,49 @@
 // vim: set ts=4 sw=4 noexpandtab :
+// please be kind, written short-notice and under covid, jvdb
 
-// load up the service worker (if a newer version is there)
-/*
-if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('/sw.js').then(() => {
-		console.log("installed service worker");
-	});
-}
-*/
-
+// events need to be this close to each other in time to be considered
+// part of the same cluster, so we can skip inter-cluster (empty) timelines (grid view)
 const clustering_window = luxon.Duration.fromISOTime('00:30');
+// every minute is this much rem (grid view)
 const minute_to_height_ratio = 0.125;
-let url_base = 'https://nogal.slechte.info/api/';
-let url_schedule = `${url_base}get_daemon_overview`; //'https://pretalx.fri3d.be/fri3dcamp2022/schedule/export/schedule.json';
-let url_mine_get = `${url_base}get_daemon_mine`; //'https://pretalx.fri3d.be/fri3dcamp2022/schedule/export/schedule.json';
-let url_mine_set = `${url_base}set_daemon_mine`; //'https://pretalx.fri3d.be/fri3dcamp2022/schedule/export/schedule.json';
-let schedule_complete = {};
+
+let url_base = '/api/';
+let url_schedule = `${url_base}get_daemon_overview`;
+let url_mine_get = `${url_base}get_daemon_mine`;
+let url_mine_set = `${url_base}set_daemon_mine`;
+
+// api-filled, list of our known volunteers
 let daemons_available = [];
+// api-filled, list of known volunteering posts
 let posts_available = [];
-let display_list = true;
+// api-filled, the main schedule
+let schedule = {
+	days : [],
+	start : undefined,
+	end : undefined,
+};
 
 document.querySelector('#js_warning').hidden = true;
 
-function load_schedule() {
-	console.log("loading schedule");
-	fetch(url_schedule).then((resp) => resp.json()).then(sched => {
-		// save posts
-		posts_available = sched.overview.posts;
-		recalc_display_method();
-		schedule_install(sched.overview);
-	});
-}
-
 addEventListener('resize', (e) => {
-	console.log('resize!');
-	recalc_display_method();
-	schedule_show();
+	schedule_blit(schedule);
 });
 
-load_schedule();
+schedule_load();
 
+/*
+                      _ _ 
+  ___ _ __ ___   __ _(_) |
+ / _ \ '_ ` _ \ / _` | | |
+|  __/ | | | | | (_| | | |
+ \___|_| |_| |_|\__,_|_|_|
+                          
+upon form entry, populate global daemons_available and re-blit the schedule
+*/
 
-function recalc_display_method() {
-	let needed = 50 + (posts_available.length * 158);
-	console.log('needed='+needed);
-	console.log("have="+window.innerWidth);
+document.querySelector('#identification_entry').addEventListener('submit', (event) => {
+	event.preventDefault();
 
-	display_list = (window.innerWidth < needed);
-}
-
-// identification
-document.querySelector('#identification_launch').addEventListener('click', (event) => {
 	let email = document.querySelector('#identification_email').value;
 
 	fetch(url_mine_get, {
@@ -72,10 +65,35 @@ document.querySelector('#identification_launch').addEventListener('click', (even
 			document.querySelector('#notify_email_error').hidden = false;
 		}
 		// redraw the schedule
-		schedule_show();
+		schedule_blit(schedule);
 	});
 });
 
+/*
+                     _       _ 
+ _ __ ___   ___   __| | __ _| |
+| '_ ` _ \ / _ \ / _` |/ _` | |
+| | | | | | (_) | (_| | (_| | |
+|_| |_| |_|\___/ \__,_|\__,_|_|
+                               
+
+there is a single <dialog> showing the assigned/available daemons (volunteers)
+for a given slot
+
+daemon_select_show() shows it by:
+	* stuffing in the daemons it finds in global daemons_available (marking them as
+	  already commited if they are already committed to this slot
+	* saving meta in the modal's dataset
+
+the close event is called by the runtime when a button is clicked on the modal, and:
+	* fetches the meta from the dataset
+	* extracts the selected daemons
+	* if there's too many daemons selected (more than the slot can hold), _show()s the modal again
+	* if all succeeds, saves changes to global daemons_available
+	* calls daemons_push
+
+daemons_push() pushes what's in global daemons_available and blanketly reloads the sched
+*/
 function daemons_push() {
 
 	let email = document.querySelector('#identification_email').value;
@@ -99,13 +117,12 @@ function daemons_push() {
 		}),
 	}).then(resp => resp.json()).then(ret => {
 		daemons_available = ret.daemons;
-		load_schedule();
+		schedule_load();
 	});
 }
 
 document.querySelector('#commit_daemons').addEventListener('close', (event) => {
 	let modal = event.target;
-	console.log(modal.returnValue);
 
 	if (modal.returnValue == 'rollback') {
 		return;
@@ -114,15 +131,12 @@ document.querySelector('#commit_daemons').addEventListener('close', (event) => {
 	let slot_id = parseInt(modal.dataset.slotId);
 	let slot_room = parseInt(modal.dataset.slotRoom);
 	let daemon_ids = [];
-	console.log(typeof slot_id);
 
 	modal.querySelectorAll('.daemon_name_select').forEach((s) => {
 		if (s.checked) {
 			daemon_ids.push(parseInt(s.dataset.daemonId));
 		}
 	});
-
-	console.log(`slot_id=${slot_id} slot_room=${slot_room} daemon_ids=${daemon_ids}`);
 
 	if (!slot_id)
 		return;
@@ -142,7 +156,9 @@ document.querySelector('#commit_daemons').addEventListener('close', (event) => {
 	});
 
 	daemons_push();
+
 });
+
 function daemon_select_show(slot_id, slot_room, message=undefined) {
 	
 	let modal = document.querySelector('#commit_daemons');
@@ -172,29 +188,36 @@ function daemon_select_show(slot_id, slot_room, message=undefined) {
 	modal.dataset.slotRoom = slot_room;
 
 	modal.showModal();
+
 }
 
-let schedule = {
-	days : [],
-	start : undefined,
-	end : undefined,
-};
-let filter = {
-	tracks : {
-		available : new Set(),
-		selected : new Set(),
-	},
-	time : {
-		min : undefined,
-		max : undefined,
-		selected : {
-			start : undefined,
-			end : undefined,
-		},
-	},
-	search : '',
-};
-function schedule_install(sched) {
+/*
+          _              _       _      
+ ___  ___| |__   ___  __| |_   _| | ___ 
+/ __|/ __| '_ \ / _ \/ _` | | | | |/ _ \
+\__ \ (__| | | |  __/ (_| | |_| | |  __/
+|___/\___|_| |_|\___|\__,_|\__,_|_|\___|
+                                        
+
+_load() loads it all up, calls _process() and saves the processed in global schedule
+_process() does a pass over the input data and basically just internalizes timestamps
+_blit() shows the current schedule in global schedule, either with
+	_show_list() for the list (smaller screens) or
+	_show_grid() for the grid
+_inflate_event() helps this by stuffing an event (id est, slot) into a html node
+*/
+
+function schedule_load() {
+
+	fetch(url_schedule).then((resp) => resp.json()).then(sched => {
+		posts_available = sched.overview.posts;
+		schedule = schedule_process(sched.overview);
+		schedule_blit(schedule);
+	});
+
+}
+
+function schedule_process(sched) {
 
 	// output
 	let o = {
@@ -259,239 +282,10 @@ function schedule_install(sched) {
 		o.days.push(d);
 	});
 
-	// install the main schedule
-	schedule = o;
-
-	// update the filtering widget
-	filter.tracks.available = available_tracks;
-	filter.tracks.selected = new Set(available_tracks.values());
-	filter.time.min = o.start;
-	filter.time.selected.start = o.start;
-	filter.time.max = o.end;
-	filter.time.selected.end = o.end;
-	
-	console.log(schedule);
-	console.log(filter);
-
-	filter_update();
-	filter_apply();
-
-	schedule_show();
-}
-
-function filter_update() {
-
-	let output = document.querySelector('#filter');
-
-	while (output.hasChildNodes()) {
-		output.removeChild(output.lastChild);
-	}
-
-	for (const t of filter.tracks.available.values()) {
-		console.log(t);
-		let b = document.createElement('input');
-
-		b.type = 'checkbox';
-		b.value = t;
-		b.name = `filter_track_selector_${t}`;
-		b.id = `filter_track_selector_${t}`;
-		b.checked = (filter.tracks.selected.has(t)) ? true : false;
-
-		b.onclick = (e) => {
-			let t = e.target.value;
-			if (e.target.checked) {
-				filter.tracks.selected.add(t);
-			} else {
-				filter.tracks.selected.delete(t);
-			}
-			filter_apply();
-		};
-
-		let l = document.createElement('label');
-		l.htmlFor = b.id;
-		l.appendChild(document.createTextNode(t));
-
-		output.appendChild(b);
-		output.appendChild(l);
-	}
-
-	let time_start = document.querySelector('#filter_time_start');
-	let time_end = document.querySelector('#filter_time_end');
-
-	function mangle_for_html(a_datetime) {
-		return a_datetime.toISO().slice(0, 16);
-	}
-	/*
-	 * the html datetime-local is given YYYY-MM-DDTHH:MM times and assumes them
-	 * to be local and shows them as such (also upon change event), so we
-	 * - set: convert internal to normal ISO local+-offset, strip :SS.sss & offset,
-	 *   insert that
-	 * - get: add :SS.sss, feed it to luxon.DateTime.fromISO() which assumes it to
-	 *   be in local time and adds the relevant offset again
-	 */
-	let t = document.createElement('input');
-	t.type = 'datetime-local';
-	t.name = 'filter_time_start';
-	t.id = 'filter_time_start';
-	t.min = mangle_for_html(filter.time.min);
-	t.max = mangle_for_html(filter.time.max);
-	t.value = mangle_for_html(filter.time.selected.start);
-	t.onchange = (e) => {
-		filter.time.selected.start = luxon.DateTime.fromISO(e.target.value + ':00.000');
-		filter_apply();
-	};
-	let l = document.createElement('label');
-	l.htmlFor = t.id;
-	l.appendChild(document.createTextNode('from'));
-	output.appendChild(l);
-	output.appendChild(t);
-
-	t = document.createElement('input');
-	t.type = 'datetime-local';
-	t.name = 'filter_time_start';
-	t.id = 'filter_time_start';
-	t.min = mangle_for_html(filter.time.min);
-	t.max = mangle_for_html(filter.time.max);
-	t.value = mangle_for_html(filter.time.selected.end);
-	t.onchange = (e) => {
-		filter.time.selected.end = luxon.DateTime.fromISO(e.target.value + ':59.999');
-		filter_apply();
-	};
-	l = document.createElement('label');
-	l.htmlFor = t.id;
-	l.appendChild(document.createTextNode('to'));
-	output.appendChild(l);
-	output.appendChild(t);
-
-	t = document.createElement('input');
-	t.type = 'text';
-	t.name = 'filter_text';
-	t.id = 'filter_text';
-	t.onchange = (e) => {
-		filter.search = e.target.value;
-		filter_apply();
-	};
-	l = document.createElement('label');
-	l.htmlFor = t.id;
-	l.appendChild(document.createTextNode('search'));
-	output.appendChild(l);
-	output.appendChild(t);
+	return o;
 
 }
 
-let schedule_filtered = {
-	days : [],
-};
-
-function filter_apply() {
-	
-	let o = {
-		days : [],
-	};
-
-	let filter_interval = luxon.Interval.fromDateTimes(
-		filter.time.selected.start,
-		filter.time.selected.end
-	);
-
-	function event_contains_string(event, search) {
-		function check(haystack, needle) {
-			return haystack && haystack.toLowerCase().indexOf(needle.toLowerCase()) > -1;
-		}
-		return [ 'abstract', 'description', 'title' ].filter(x => check(event[x], search)).length > 0;
-	}
-
-	schedule.days.forEach((day) => {
-	
-		// cull days not in our filtering time range
-		if (!filter_interval.overlaps(day.timespan))
-			return;
-
-		let d = {
-			tracks : [],
-			timespan : day.timespan,
-		};
-
-		day.tracks.forEach((track) => {
-
-			// cull unselected tracks
-			if (!filter.tracks.selected.has(track.post))
-				return;
-
-			let t = {
-				post: track.post,
-				events : [],
-			};
-
-			track.events.forEach((event) => {
-				// cull out not within filtering time range
-				if (!filter_interval.overlaps(event.timespan))
-					return;
-
-				// cull out not matching search text, if set
-				if (filter.search.length && !event_contains_string(event.meta, filter.search))
-					return;
-
-				t.events.push(event);
-
-			});
-
-			d.tracks.push(t);
-
-		});
-
-		o.days.push(d);
-
-	});
-
-	schedule_filtered = o;
-
-	schedule_show();
-
-}
-
-function template_spin(template_selector, id=undefined) {
-
-	let template = document.querySelector(template_selector);
-	let instance = template.content.cloneNode(true);
-
-	if (id) {
-		// if we need to set an id, we need a wrapper to contain whatever
-		// the template contains, and assign the id to that wrapper
-		let wrapper = document.createElement('div');
-		wrapper.id = id;
-		wrapper.appendChild(instance);
-		instance = wrapper;
-	}
-
-	return instance;
-
-}
-
-function schedule_show() {
-	if (display_list)
-		schedule_show_list(schedule_filtered);
-	else
-		schedule_show_grid(schedule_filtered);
-}
-
-// round a luxon datetime to 30mins
-function round_datetime(dt) {
-	let hour = dt.hour;
-	let min = dt.minute;
-
-	if (dt.minute < 15) {
-		min = 0;
-	} else if (dt.minute < 45) {
-		min = 30;
-	} else {
-		min = 0;
-		hour += 1;
-	}
-
-	return dt.set({ hour : hour, minute : min, second : 0 });
-
-}
 
 // given an html node, stuff the event's details into it, if the event's id
 // is in slots_committed we show this visually too
@@ -543,6 +337,15 @@ function schedule_inflate_event(node, event, slots_committed) {
 		});
 	}
 
+}
+
+// show the main schedule, in list/grid for depending on screen real estate
+function schedule_blit(schedule) {
+	let needed = 50 + (posts_available.length * 158);
+
+	let blit = (window.innerWidth < needed) ? schedule_show_list : schedule_show_grid;
+
+	blit(schedule);
 }
 
 function schedule_show_list(schedule) {
@@ -622,7 +425,7 @@ function schedule_show_grid(schedule) {
 		day.tracks.forEach((track) => {
 			time_clusters = luxon.Interval.merge(
 				time_clusters.concat(
-					track.events.map((e) => 
+					track.events.map((e) =>
 						luxon.Interval.fromDateTimes(
 							e.timespan.start,
 							e.timespan.end.plus(clustering_window)
@@ -634,7 +437,6 @@ function schedule_show_grid(schedule) {
 
 		time_clusters.sort((l, r) => l.start - r.start);
 
-		console.log("clusters:");
 		time_clusters.forEach((cluster) => {
 			let ruler = document.createElement('div');
 			ruler.classList.add('ruler');
@@ -720,6 +522,53 @@ function schedule_show_grid(schedule) {
 
 		day_header.querySelector('.list_day_content').appendChild(day_output);
 		output.appendChild(day_header);
-		//output.appendChild(day_output);
 	});
+
+}
+
+/*
+       _   _ _ 
+ _   _| |_(_) |
+| | | | __| | |
+| |_| | |_| | |
+ \__,_|\__|_|_|
+               
+*/
+
+// instantiate a <template> id'd by the selector, optionally assigning
+// the instance the specified id (technically, a <div id="whatyouwant"></div> around it)
+function template_spin(template_selector, id=undefined) {
+
+	let template = document.querySelector(template_selector);
+	let instance = template.content.cloneNode(true);
+
+	if (id) {
+		// if we need to set an id, we need a wrapper to contain whatever
+		// the template contains, and assign the id to that wrapper
+		let wrapper = document.createElement('div');
+		wrapper.id = id;
+		wrapper.appendChild(instance);
+		instance = wrapper;
+	}
+
+	return instance;
+
+}
+
+// round a luxon datetime to 30mins
+function round_datetime(dt) {
+	let hour = dt.hour;
+	let min = dt.minute;
+
+	if (dt.minute < 15) {
+		min = 0;
+	} else if (dt.minute < 45) {
+		min = 30;
+	} else {
+		min = 0;
+		hour += 1;
+	}
+
+	return dt.set({ hour : hour, minute : min, second : 0 });
+
 }

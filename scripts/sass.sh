7ab7419bab00a6d208ee5e047d8c1c82c7b98620
Toon Van de Putte <toon@automaton.be>
2022-08-03 16:49:45 +0200

    sassified css + styling tweaks

--- scripts/sass.sh
+++ scripts/sass.sh
@@ -1,3 +1,5 @@
 sass ../fr1ckets/src/fr1ckets/sass/fri3d2020_overrides.scss ../fr1ckets/src/fr1ckets/static/css/fri3d2020_overrides.css
 
 sass ../fr1ckets/src/fr1ckets/sass/vanilla.scss ../fr1ckets/src/fr1ckets/static/css/vanilla.css
+
+sass ../fr1ckets/src/fr1ckets/sass/daemons.scss ../fr1ckets/src/fr1ckets/static/daemons/css/index.css
